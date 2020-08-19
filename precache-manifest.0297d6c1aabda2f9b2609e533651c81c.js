3bdfbe1d7a8fff6906192ad28f28487dc5fa2f75
AlexDjon <alexdjonata30@gmail.com>
2020-08-19 20:36:22 -0300

    Primeira Build - Testando Host

---
+++ precache-manifest.0297d6c1aabda2f9b2609e533651c81c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a8deb5e06b89d484c08e78046d2404",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40e906f91f392109ef18",
+    "url": "/static/css/main.12c04dab.chunk.css"
+  },
+  {
+    "revision": "590b3d755a928dd0b455",
+    "url": "/static/js/2.00b8de8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.00b8de8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e906f91f392109ef18",
+    "url": "/static/js/main.9b75fa0a.chunk.js"
+  },
+  {
+    "revision": "1cf397eb45bd7fbf3c68",
+    "url": "/static/js/runtime-main.72853330.js"
+  },
+  {
+    "revision": "02d4c0fa0752d53b26cd1b45d0042505",
+    "url": "/static/media/default-user.02d4c0fa.png"
+  }
+]);
